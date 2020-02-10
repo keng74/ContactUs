913c72d04bb1bcd22525aada30ceec799187f7f2
keng74 <60871972+keng74@users.noreply.github.com>
2020-02-10 16:30:53 +0800

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+function doAlert(){
+    /*alert('Hi');
+    myMessage('Hello');*/
+    
+    var myVariable;
+    myVariable="Success";
+    mySeondMessage(myVariable);
+
+   /* var myData = ['One','Two','Three'];
+    myThirdMessage(myData[1]);*/
+}
+
+function myMessage(a){
+    alert(a);
+}
+
+function mySeondMessage(a){
+    alert(a);
+}
+
+function myThirdMessage(a){
+    alert(a);
+}
